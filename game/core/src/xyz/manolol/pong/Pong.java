ec71c2a8a16eedeeb2447a800168ea4eecc5baa3
Mario Ebenhofer <20230009@students.htl-perg.ac.at>
2024-06-10 12:34:47 +0200

    continue setup

--- game/core/src/xyz/manolol/pong/Pong.java
+++ game/core/src/xyz/manolol/pong/Pong.java
@@ -1,31 +1,14 @@
 package xyz.manolol.pong;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.Game;
+import xyz.manolol.pong.screens.game.GameScreen;
 
-public class Pong extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+public class Pong extends Game {
+	public static Pong GAME;
 
-	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	public void create() {
+		GAME = this;
+
+		GAME.setScreen(new GameScreen());
 	}
 }
---
+++ game/core/src/xyz/manolol/pong/screens/game/GameScreen.java
@@ -0,0 +1,7 @@
+package xyz.manolol.pong.screens.game;
+
+import com.badlogic.gdx.ScreenAdapter;
+
+public class GameScreen extends ScreenAdapter {
+
+}
