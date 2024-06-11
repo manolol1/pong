382163c0de7ae02892684de7901720f859be30c4
Mario Ebenhofer <manolol@mailbox.org>
2024-06-11 22:08:25 +0200

    add FontManager
    for later

---
+++ game/core/src/xyz/manolol/pong/utils/FontManager.java
@@ -0,0 +1,40 @@
+package xyz.manolol.pong.utils;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.utils.Disposable;
+import com.badlogic.gdx.utils.ObjectMap;
+
+public class FontManager implements Disposable {
+
+    private final FreeTypeFontGenerator fontGenerator;
+    private final FreeTypeFontGenerator.FreeTypeFontParameter parameter;
+    private final ObjectMap<Integer, BitmapFont> fonts;
+    public FontManager(String filePath) {
+        this.fontGenerator = new FreeTypeFontGenerator(Gdx.files.internal(filePath));
+        this.parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        this.fonts = new ObjectMap<>();
+    }
+
+    public BitmapFont getFont(int size) {
+        // generate new font if the specific size hasn't been generated yet
+        if (!fonts.containsKey(size)) {
+            parameter.size = size;
+            parameter.minFilter = Texture.TextureFilter.Linear;
+            parameter.magFilter = Texture.TextureFilter.Linear;
+            fonts.put(size, fontGenerator.generateFont(parameter));
+        }
+        return fonts.get(size);
+    }
+
+    @Override
+    public void dispose() {
+        for (BitmapFont font : fonts.values()) {
+            if (font != null) {
+                font.dispose();
+            }
+        }
+    }
+}
