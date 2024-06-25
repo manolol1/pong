c87bf90da74ba1a0e0083e49f234075660a994ba
Mario Ebenhofer <manolol@mailbox.org>
2024-06-25 17:05:38 +0200

    add music

--- game/core/src/xyz/manolol/pong/Pong.java
+++ game/core/src/xyz/manolol/pong/Pong.java
@@ -1,6 +1,8 @@
 package xyz.manolol.pong;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 import com.kotcrab.vis.ui.VisUI;
 import xyz.manolol.pong.screens.mainmenu.MainMenuScreen;
 import xyz.manolol.pong.screens.start.StartScreen;
@@ -8,10 +10,22 @@ import xyz.manolol.pong.screens.start.StartScreen;
 public class Pong extends Game {
 	public static Pong GAME;
 
+	private Music music;
+
 	public void create() {
 		GAME = this;
 		VisUI.load();
 		Constants.load();
 		GAME.setScreen(new MainMenuScreen());
+
+		music = Gdx.audio.newMusic(Gdx.files.internal("music/pong.ogg"));
+		music.setLooping(true);
+		music.play();
+	}
+
+	public void dispose() {
+		super.dispose();
+		VisUI.dispose();
+		music.dispose();
 	}
 }
